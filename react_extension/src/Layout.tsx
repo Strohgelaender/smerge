ef3a09c3300d15cd4d50708d14eb240254345a72
lucas <ga53foy@mytum.de>
2026-03-08 18:07:03 +0100

    Logout nicht zeigen wenn nicht angemeldet um Fehler zu vermeiden

--- react_extension/src/Layout.tsx
+++ react_extension/src/Layout.tsx
@@ -10,199 +10,263 @@ import ListItemText from "@mui/material/ListItemText";
 import MenuIcon from "@mui/icons-material/Menu";
 import Toolbar from "@mui/material/Toolbar";
 import Button from "@mui/material/Button";
-import { useEffect, useState } from "react";
+import Menu from "@mui/material/Menu";
+import MenuItem from "@mui/material/MenuItem";
+import KeyboardArrowDownIcon from "@mui/icons-material/KeyboardArrowDown";
+import React, {useEffect, useState} from "react";
 
 import "./Layout.css";
-import { useTranslation } from "react-i18next";
-import httpService from "./services/HttpService";
-import { useNavigate } from "react-router-dom";
-import { getCurrentUser, logout } from "./services/TeacherAuthService";
+import {useTranslation} from "react-i18next";
+import {getCurrentUser, logout} from "./services/TeacherAuthService";
 
 // function Layout() {
 
 const drawerWidth = 240;
 const navItems = [
-  {
-    name: "main.nav_howto",
-    foo: () => {
-      window.location.href = "/howto";
+    {
+        name: "main.nav_howto",
+        foo: () => {
+            window.location.href = "/howto";
+        },
     },
-  },
-  {
-    name: "main.nav_impressum",
-    foo: () => {
-      window.location.href = "/impressum";
+    {
+        name: "main.nav_impressum",
+        foo: () => {
+            window.location.href = "/impressum";
+        },
     },
-  },
-  {
-    name: "main.nav_back",
-    foo: () => {
-      window.history.back();
+    {
+        name: "main.nav_back",
+        foo: () => {
+            window.history.back();
+        },
     },
-  },
-  {
-    name: getCurrentUser()? "Teacher-View" : "main.nav_teacherlogin",
-    foo: () => {
-        // TODO remove /ext once migration is finished
-      const destination = getCurrentUser()? "ext/teacher_view" : "ext/teacher_login";
-      if(window.location.href.includes("/ext")){
-        window.location.href = window.location.href.split("ext")[0] + destination;
-      }
-      else {
-        window.location.href = destination
-      }
+    {
+        name: getCurrentUser() ? "main.nav_teacher_view" : "main.nav_teacherlogin",
+        foo: () => {
+            // TODO remove /ext once migration is finished
+            const destination = getCurrentUser() ? "ext/teacher_view" : "ext/teacher_login";
+            if (window.location.href.includes("/ext")) {
+                window.location.href = window.location.href.split("ext")[0] + destination;
+            } else {
+                window.location.href = destination
+            }
+        }
+    },
+    {
+        display: !!getCurrentUser(),
+        name: getCurrentUser() ? "main.nav_logout" : "",
+        foo: () => {
+            if (window.location.href.includes("/ext") && getCurrentUser()) {
+                logout();
+                window.location.href = window.location.href.split("ext")[0] + "ext/teacher_login";
+            }
+        }
     }
-  },
-  {
-    name: getCurrentUser()? "Logout" : "",
-    foo: () => {
-      if(window.location.href.includes("/ext") && getCurrentUser()){
-        logout();
-        window.location.href = window.location.href.split("ext")[0] + "ext/teacher_login";
-      }
-    }
-  }
 ];
 
 function Layout() {
-  const [mobileOpen, setMobileOpen] = useState(false);
+    const [mobileOpen, setMobileOpen] = useState(false);
+    const [languageAnchorEl, setLanguageAnchorEl] = useState<null | HTMLElement>(null);
 
-  const handleDrawerToggle = () => {
-    setMobileOpen((prevState) => !prevState);
-  };
+    const handleDrawerToggle = () => {
+        setMobileOpen((prevState) => !prevState);
+    };
 
-  const { t } = useTranslation();
+    const {t, i18n} = useTranslation();
+    const currentLanguage = i18n.resolvedLanguage?.startsWith("de") ? "de" : "en";
 
-  // check if csfr token was set and redirect to missing page if not
-  const navigate = useNavigate();
+    const setLanguage = (lang: "de" | "en") => {
+        if (currentLanguage !== lang) {
+            void i18n.changeLanguage(lang);
+        }
+        setLanguageAnchorEl(null);
+    };
 
-  // TODO: New mechnaism to enuse security when full client is in react (no token)
+    const openLanguageMenu = (event: React.MouseEvent<HTMLElement>) => {
+        setLanguageAnchorEl(event.currentTarget);
+    };
 
-  /*useEffect(() => {
-    if (
-      httpService.csrftoken === "" &&
-      !location.href.includes("csfr_missing")
-    ) {
-      // react router to missing page
-      console.log(
-        location.href.split("/")[location.href.split("/").length - 1]
-      );
-      const lastPart =
-        location.href.split("/")[location.href.split("/").length - 1];
-      if (lastPart.includes("csfr_missing")) {
-        navigate("/ext/csfr_missing");
-      } else {
-        navigate("/ext/csfr_missing/" + lastPart);
+    const closeLanguageMenu = () => {
+        setLanguageAnchorEl(null);
+    };
+
+    // check if csfr token was set and redirect to missing page if not
+
+    // TODO: New mechnaism to enuse security when full client is in react (no token)
+
+    // const navigate = useNavigate();
+    /*useEffect(() => {
+      if (
+        httpService.csrftoken === "" &&
+        !location.href.includes("csfr_missing")
+      ) {
+        // react router to missing page
+        console.log(
+          location.href.split("/")[location.href.split("/").length - 1]
+        );
+        const lastPart =
+          location.href.split("/")[location.href.split("/").length - 1];
+        if (lastPart.includes("csfr_missing")) {
+          navigate("/ext/csfr_missing");
+        } else {
+          navigate("/ext/csfr_missing/" + lastPart);
+        }
       }
-    }
-  // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);*/
-  navItems.push();
-  const container = window.document.body;
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, []);*/
+    navItems.push();
+    const container = window.document.body;
 
-  const drawer = (
-    <Box onClick={handleDrawerToggle} sx={{ textAlign: "center" }}>
-      <List>
-        {navItems.map((item) => (
-          <ListItem key={item.name} disablePadding>
-            <ListItemButton sx={{ textAlign: "center" }} onClick={item.foo}>
-              <ListItemText primary={t(item.name)} className="nav_text" />
-            </ListItemButton>
-          </ListItem>
-        ))}
-      </List>
-    </Box>
-  );
-
-  return (
-    <Box sx={{ display: "flex", paddingLeft: "0px", height: "64px" }}>
-      <CssBaseline />
-      <AppBar
-        component="nav"
-        sx={{ pl: "0px" }}
-        style={{ height: "64px", background: "rgb(15,3,3)" }}
-      >
-        <Toolbar
-          sx={{
-            width: "100%",
-            paddingLeft: "0px !important",
-            paddingRight: "1.8% !important",
-            background: "rgb(15,3,3);",
-          }}
-        >
-          <a href="/" className="logo">
-            {document.title.includes("DEV")
-              ? "SMERGE (DEV)"
-              : document.title.includes("BETA")
-              ? "SMERGE (BETA)"
-              : "SMERGE"}
-          </a>
-          <Box
-            sx={{
-              flexGrow: 1,
-              display: {
-                xs: "none",
-                sm: "flex",
-                paddingRight: "11px",
-                paddingBottom: "0px",
-              },
-              justifyContent: "end",
-            }}
-          >
-            {navItems.map((item) => (
-              <Button
-                className="nav_text"
-                key={item.name}
-                sx={{ color: "#fff" }}
-                onClick={item.foo}
-              >
-                {t(item.name)}
-              </Button>
-            ))}
-          </Box>
-          <Box
-            sx={{
-              flexGrow: 1,
-              display: { xs: "flex", sm: "none" },
-              justifyContent: "end",
-            }}
-            style={{ height: "64px" }}
-          >
-            <IconButton
-              color="inherit"
-              aria-label="open drawer"
-              edge="start"
-              onClick={handleDrawerToggle}
-              sx={{ mr: 2, display: { sm: "none" } }}
+    const languageSwitch = (
+        <>
+            <Button
+                size="small"
+                variant="outlined"
+                onClick={openLanguageMenu}
+                endIcon={<KeyboardArrowDownIcon/>}
+                sx={{
+                    minWidth: "72px",
+                    color: "white",
+                    borderColor: "rgba(255,255,255,0.4)",
+                    mr: 1,
+                    "&:hover": {borderColor: "white", backgroundColor: "rgba(255,255,255,0.08)"},
+                }}
             >
-              <MenuIcon fontSize={"large"} />
-            </IconButton>
-          </Box>
-        </Toolbar>
-      </AppBar>
-      <nav>
-        <Drawer
-          container={container}
-          variant="temporary"
-          anchor="right"
-          open={mobileOpen}
-          onClose={handleDrawerToggle}
-          ModalProps={{
-            keepMounted: true, // Better open performance on mobile.
-          }}
-          sx={{
-            display: { xs: "block", sm: "none" },
-            "& .MuiDrawer-paper": {
-              boxSizing: "border-box",
-              width: drawerWidth,
-            },
-          }}
-        >
-          {drawer}
-        </Drawer>
-      </nav>
-    </Box>
-  );
+                {currentLanguage.toUpperCase()}
+            </Button>
+            <Menu
+                anchorEl={languageAnchorEl}
+                open={Boolean(languageAnchorEl)}
+                onClose={closeLanguageMenu}
+            >
+                <MenuItem selected={currentLanguage === "de"} onClick={() => setLanguage("de")}>
+                    Deutsch
+                </MenuItem>
+                <MenuItem selected={currentLanguage === "en"} onClick={() => setLanguage("en")}>
+                    English
+                </MenuItem>
+            </Menu>
+        </>
+    );
+
+    const drawer = (
+        <Box sx={{textAlign: "center"}}>
+            <Box sx={{display: "flex", justifyContent: "center", py: 2}}>
+                {languageSwitch}
+            </Box>
+            <List>
+                {navItems.map((item) => {
+                    if (!item.display) {
+                        return <></>;
+                    }
+                    return <ListItem key={item.name} disablePadding>
+                        <ListItemButton
+                            sx={{textAlign: "center"}}
+                            onClick={() => {
+                                item.foo();
+                                handleDrawerToggle();
+                            }}
+                        >
+                            <ListItemText primary={t(item.name)} className="nav_text"/>
+                        </ListItemButton>
+                    </ListItem>;
+                })}
+            </List>
+        </Box>
+    );
+
+    return (
+        <Box sx={{display: "flex", paddingLeft: "0px", height: "64px"}}>
+            <CssBaseline/>
+            <AppBar
+                component="nav"
+                sx={{pl: "0px"}}
+                style={{height: "64px", background: "rgb(15,3,3)"}}
+            >
+                <Toolbar
+                    sx={{
+                        width: "100%",
+                        paddingLeft: "0px !important",
+                        paddingRight: "1.8% !important",
+                        background: "rgb(15,3,3);",
+                    }}
+                >
+                    <a href="/" className="logo">
+                        {document.title.includes("DEV")
+                            ? "SMERGE (DEV)"
+                            : document.title.includes("BETA")
+                                ? "SMERGE (BETA)"
+                                : "SMERGE"}
+                    </a>
+                    <Box
+                        sx={{
+                            flexGrow: 1,
+                            display: {
+                                xs: "none",
+                                sm: "flex",
+                                paddingRight: "11px",
+                                paddingBottom: "0px",
+                            },
+                            justifyContent: "end",
+                            alignItems: "center",
+                        }}
+                    >
+                        {languageSwitch}
+                        {navItems.map((item) => (
+                            <Button
+                                className="nav_text"
+                                key={item.name}
+                                sx={{color: "#fff"}}
+                                onClick={item.foo}
+                            >
+                                {t(item.name)}
+                            </Button>
+                        ))}
+                    </Box>
+                    <Box
+                        sx={{
+                            flexGrow: 1,
+                            display: {xs: "flex", sm: "none"},
+                            justifyContent: "end",
+                        }}
+                        style={{height: "64px"}}
+                    >
+                        <IconButton
+                            color="inherit"
+                            aria-label="open drawer"
+                            edge="start"
+                            onClick={handleDrawerToggle}
+                            sx={{mr: 2, display: {sm: "none"}}}
+                        >
+                            <MenuIcon fontSize={"large"}/>
+                        </IconButton>
+                    </Box>
+                </Toolbar>
+            </AppBar>
+            <nav>
+                <Drawer
+                    container={container}
+                    variant="temporary"
+                    anchor="right"
+                    open={mobileOpen}
+                    onClose={handleDrawerToggle}
+                    ModalProps={{
+                        keepMounted: true, // Better open performance on mobile.
+                    }}
+                    sx={{
+                        display: {xs: "block", sm: "none"},
+                        "& .MuiDrawer-paper": {
+                            boxSizing: "border-box",
+                            width: drawerWidth,
+                        },
+                    }}
+                >
+                    {drawer}
+                </Drawer>
+            </nav>
+        </Box>
+    );
 }
 
 export default Layout;
