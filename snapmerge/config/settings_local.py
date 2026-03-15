22cfe760703f9c3334323c9c6af21019ce922db3
lucas <ga53foy@mytum.de>
2026-03-15 16:11:31 +0100

    Admin-Ansicht behoben.

--- snapmerge/config/settings_local.py
+++ snapmerge/config/settings_local.py
@@ -4,6 +4,8 @@ import json
 URL = "http://127.0.0.1:8000"
 POST_BACK_URL = "http://127.0.0.1"
 
+SITE_ID = 1
+
 # SECRET_KEY = '...'
 # EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
